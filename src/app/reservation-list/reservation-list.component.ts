7549421ce50843159c9d318a38af4e666be27ba3
Nikhil Jalagam <nikyjalagam@gmail.com>
2025-02-25 22:05:31 -0500

    final

--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css']
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+ 
+  reservations: Reservation[] = [];
 
+
+  constructor(private reservationService: ReservationService){
+
+  }
+
+  ngOnInit(): void {
+    this.reservationService.getReservations().subscribe(reservations =>
+      this.reservations = reservations
+    );
+  }
+
+  deleteReservation(id: String){
+    this.reservationService.deleteReservation(id).subscribe(() => {
+      console.log("Delete request processed")
+    });
+  }
+  
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:3001"
+  private reservations : Reservation[] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+  
+  getReservations(): Observable<Reservation[]>{
+    return this.http.get<Reservation[]>(this.apiUrl+"/reservations");
+  }
+
+  getReservation(id : String): Observable<Reservation>{
+    return this.http.get<Reservation>(this.apiUrl+"/reservation/"+id);
+  }
+
+  addReservation(reservation: Reservation): Observable<void>{
+    return this.http.post<void>(this.apiUrl+"/reservation", reservation);   
+    
+  }
+
+  deleteReservation(id: String): Observable<void>{
+
+    return this.http.delete<void>(this.apiUrl+"/reservation/"+id);    
+  }
+
+  updateReservation(id:String, updatedReservation : Reservation) :Observable<void>{
+    return this.http.put<void>(this.apiUrl+"/reservation/" + id, updatedReservation);   
+    
+  }
+
 }
